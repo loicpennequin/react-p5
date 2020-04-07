6118c8ba4ac6c4668c57fb55b734b860bdc004e4
Loïc <lo.pennequin@gmail.com>
2020-04-07 22:13:44 +0200

    fix 404 page on gh-pages (wip)

---
+++ example/build/precache-manifest.cb6b612da1806f70c69d0496d7ff61eb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e26aa95198ae539abb6ba192b30ce2",
+    "url": "/react-p5/index.html"
+  },
+  {
+    "revision": "3b6a1c7beaf1ce50af4c",
+    "url": "/react-p5/static/css/main.99b52ade.chunk.css"
+  },
+  {
+    "revision": "d607fc3200515e023ccb",
+    "url": "/react-p5/static/js/0.35624e6b.chunk.js"
+  },
+  {
+    "revision": "064a9966961f7b8051a54b910a7eb515",
+    "url": "/react-p5/static/js/0.35624e6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "768298562fdff278d083",
+    "url": "/react-p5/static/js/7.b6326394.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-p5/static/js/7.b6326394.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b3203f12cb51df68fea",
+    "url": "/react-p5/static/js/body.10805e0c.chunk.js"
+  },
+  {
+    "revision": "2d64a33d5ce26b42edb2",
+    "url": "/react-p5/static/js/home.page.119031f9.chunk.js"
+  },
+  {
+    "revision": "87a855100c9cc26499f8",
+    "url": "/react-p5/static/js/layer.page.a4bd280f.chunk.js"
+  },
+  {
+    "revision": "3b6a1c7beaf1ce50af4c",
+    "url": "/react-p5/static/js/main.8f037f3a.chunk.js"
+  },
+  {
+    "revision": "38664a92206b27646a35",
+    "url": "/react-p5/static/js/runtime-main.17a46277.js"
+  },
+  {
+    "revision": "13aedcb25c49ed937f2a",
+    "url": "/react-p5/static/js/smiley.page.d8496562.chunk.js"
+  }
+]);
--- example/build/service-worker.js
+++ example/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d72ac09e67669cfe002b6c879cded3d1.js"
+  "/react-p5/precache-manifest.cb6b612da1806f70c69d0496d7ff61eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-p5/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
